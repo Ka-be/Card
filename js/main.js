a3dc66cd4747f528e2fabaeb33ee3d690d8129fb
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2022-04-13 22:14:09 +0200

    Algo de la checkbox switch dark / light -> Validé

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 // MODIFICATION BG CHECKBOX 
 
-var checkbox = document.getElementById('toggle');
+var checkbox = document.getElementById('checkbox'); //Récupération du statut de la checkbox
+var label = document.getElementById('label'); //Récupération du statut du label
 
-checkbox.addEventListener('change', (event) => {
-  if (event.currentTarget.checked) {
-    checkbox.classList.add("switch_light");
-    console.log(checkbox); //Pour contrôle --> Ajoute classe a l'input pas au label
-  } else {
-    alert('not checked');
-  }
+checkbox.addEventListener('change', (event) => { //Quand la checkbox change de statut
+    if (event.currentTarget.checked) { //Si la checkbox est checkée
+        label.classList.add("switch_light"); //Alors ajoute la classe switch_light au label
+        console.log(checkbox); //Pour contrôle 
+    } 
+    else {
+        label.classList.remove("switch_light"); //Sinon, enlève la classe switch_light
+        console.log(checkbox); //Pour contrôle
+    }
 })
