f2d695aaf5bb44ca4c43a46ddfc7f917e8cef498
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2022-04-14 10:58:03 +0200

    Add script for main // + transition on main_dark & main_light

--- js/main.js
+++ js/main.js
@@ -2,14 +2,17 @@
 
 var checkbox = document.getElementById('checkbox'); //Récupération du statut de la checkbox
 var label = document.getElementById('label'); //Récupération du statut du label
+var main = document.getElementById('main') // Récup statut main
 
 checkbox.addEventListener('change', (event) => { //Quand la checkbox change de statut
     if (event.currentTarget.checked) { //Si la checkbox est checkée
         label.classList.add("switch_light"); //Alors ajoute la classe switch_light au label
+        main.classList.add("main_light"); // Alors ajoute la classe main-light au main
         console.log(checkbox); //Pour contrôle 
     } 
     else {
         label.classList.remove("switch_light"); //Sinon, enlève la classe switch_light
+        main.classList.remove("main_light"); //Sinon enlève la classe main-light au main
         console.log(checkbox); //Pour contrôle
     }
 })
